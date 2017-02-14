61b72dedb05e58d25d5ab555181aed394d3e35f5
Angel Casado <acasado@acm.org>
2017-02-14 19:40:21 +0100

    add installation on a Virtual Box

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash 
 
-cd /var/www/public
-cd main/libfap-1.5/deb
-sudo dpkg - i lib*amd64.deb
-cd /var/www/public
+cd /var/www/public/main/libfap-1.5/deb
+sudo dpkg -i lib*amd64.deb
+cd /var/www/public/main
 sudo apt-get install python-dev python-pip
 sudo pip install ephem pytz geopy configparser
 sudo mkdir /etc/local
 sudo cp config.template /etc/local/SWSconfig.ini
-sqlite3 SWiface.db <main/*.sql
+cd /var/www/public/
+cp configtail.template configtail.txt
+python genconfig.py
+rm      SWiface.db
+sqlite3 SWiface.db         < DBschema.sql
+mysql --database scotchbox < DBschema.sql
