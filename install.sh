0328aa2b80e84602d5b1bf46d98a68afe99dc4be
Angel Casado <acasado@acm.org>
2017-02-14 23:50:57 +0100

    add installation on a Virtual Box

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get -y upgrade
 cd /var/www/public/main/libfap-1.5/deb
 sudo dpkg -i lib*amd64.deb
 cd /var/www/public/main
-sudo apt-get install python-dev python-pip
+sudo apt-get install -y python-dev python-pip
 sudo pip install ephem pytz geopy configparser
 sudo mkdir /etc/local
 sudo cp config.template /etc/local/SWSconfig.ini
