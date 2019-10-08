d49ec3be145b3c0668fdbe543b9e6f34f28fefeb
Angel Casado <acasadoalonso@gmail.com>
2019-10-08 18:40:51 +0200

    add flarmfuncs to the list

--- sscommit.sh
+++ sscommit.sh
@@ -1,6 +1,7 @@
 rm *funcs.py kglid.py 
 cp /nfs/OGN/src/funcs/parserfuncs.py .
 cp /nfs/OGN/src/funcs/ogntfuncs.py .
+cp /nfs/OGN/src/funcs/flarmfuncs.py .
 cp /nfs/OGN/src/kglid.py .
 git add .
 git commit
--- sslink.sh
+++ sslink.sh
@@ -2,4 +2,5 @@ rm *funcs.py kglid.py
 ln -s /nfs/OGN/src/kglid.py .
 ln -s /nfs/OGN/src/funcs/parserfuncs.py .
 ln -s /nfs/OGN/src/funcs/ogntfuncs.py .
+ln -s /nfs/OGN/src/funcs/flarmfuncs.py .
 ls -la *funcs.py 
