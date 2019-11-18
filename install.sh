21f19ac7bfab7c312e06a76991ee0167bc54d431
Angel Casado <acasadoalonso@gmail.com>
2019-11-19 00:59:34 +0100

    some adjustments

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ sudo apt-get install -y mysql-server mysql-client sqlite3	#
 sudo mysql_secure_installation					#
 sudo apt-get install -y python3-dev python3-pip python3-mysqldb #
 sudo apt-get install -y dos2unix libarchive-dev	 autoconf mc	#
-sudo apt-get install -y pkg-config git mutt git-core		#
+sudo apt-get install -y pkg-config git mutt git-core vim	#
 sudo apt-get install -y apache2 php php-mcrypt php-mysql php-cli #
 sudo apt-get install -y php-mbstring php-gettext		#
 sudo apt-get install -y mailutils ntpdate mutt	ssmtp		#
