cbe749b10ca18f8b24ebaaf9aedd6e26c604e019
Angel Casado <acasadoalonso@gmail.com>
2019-12-01 11:37:39 +0100

    sync

--- sh/SWweekly.sh
+++ sh/SWweekly.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 dir='/nfs/OGN/DIRdata'
-db='/nfs/OGN/DIRdata/SAROGN.db'
+db ='/nfs/OGN/DIRdata/SAROGN.db'
 MySQL='NO'
-server="localhost"
+if [ $# -eq  0 ]; then
+	server='localhost'
+else
+	server=$1
+	$MySQL='YES'
+fi
+
 # test if directory is available
 if [ ! -d $dir ]
 then
@@ -22,6 +28,7 @@ then
 	echo ".dump GLIDERS" |        sqlite3 $db >GLIDERS.dump
 else
 	wget repoogn.ddns.net:60080/files/GLIDERS.dump -o GLIDERS.out
+	rm GLIDERS.out
 fi
 echo "drop table GLIDERS;" |  sqlite3 SWiface.db              	>>SWproc.log 
 sqlite3                               SWiface.db <GLIDERS.dump 	>>SWproc.log
@@ -37,15 +44,15 @@ cd ..
 if [ $MySQL  == 'YES' ]
 then
 
-	echo "drop table GLIDERS;" | mysql --login-path=SARogn -h $server SWIFACE                       >>SWproc.log 
+	echo "drop table GLIDERS;" | mysql --login-path=SARogn -h $server SWIFACE       >>SWproc.log 
 	python3 /nfs/OGN/src/SARsrc/sql* <GLIDERS.dump >gliders.sql
-	mysql --login-path=SARogn -h $server SWIFACE  <gliders.sql                      >>SWproc.log
+	mysql --login-path=SARogn -h $server SWIFACE   <gliders.sql                     >>SWproc.log
 	mysql --login-path=SARogn -h $server -e "select count(*) from GLIDERS" SWIFACE  >>SWproc.log
 	date                                                    >>SWproc.log
 	rm gliders.sql
 	echo "============= end MySQL ============================" 	>>SWproc.log
 fi
 echo "=============="$(hostname)"==========================="         	>>SWproc.log
-rm GLIDERS.dump  GLIDERS.out
+rm GLIDERS.dump  
 cd
 
