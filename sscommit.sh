401c08fd8d3e4b0033233a3d9d822f08807bc20c
Angel Casado <acasadoalonso@gmail.com>
2020-09-18 10:38:24 +0200

    sync

--- sscommit.sh
+++
@@ -1,9 +0,0 @@
-rm *funcs.py 
-cp /nfs/OGN/src/funcs/parserfuncs.py .
-cp /nfs/OGN/src/funcs/ogntfuncs.py .
-cp /nfs/OGN/src/funcs/ognddbfuncs.py .
-cp /nfs/OGN/src/funcs/flarmfuncs.py .
-git add .
-git commit
-git push origin master
-git push glidernet master
--- sslink.sh
+++
@@ -1,6 +0,0 @@
-rm *funcs.py 
-ln -s /nfs/OGN/src/funcs/parserfuncs.py .
-ln -s /nfs/OGN/src/funcs/ogntfuncs.py .
-ln -s /nfs/OGN/src/funcs/ognddbfuncs.py .
-ln -s /nfs/OGN/src/funcs/flarmfuncs.py .
-ls -la *funcs.py 
