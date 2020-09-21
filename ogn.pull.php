81fa5a8260726c3ffb7d2daaffdd5e63eca97b32
Angel Casado <acasadoalonso@gmail.com>
2020-09-21 10:23:30 +0200

    sync

--- ogn.pull.php
+++ ogn.pull.php
@@ -4,12 +4,14 @@
 
 if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
         ob_start();
+        passthru("chmod 775 -R * .gi* ");
         passthru("/usr/bin/git --no-pager pull origin master");
         $var = ob_get_contents();
+        echo "RC=".$var." \n";
+        passthru("chmod 775 -R * .gi* ");
         passthru("touch UPDATED.by.GIT");
         ob_end_clean(); 
-        echo "RC=".$var."\n";
-        //echo "git pull done\n\n";
 }
+echo date("Y-m-d H:i:s")."\n";
 //echo var_dump($_SERVER);
-?>hi git pull done !!!
+?>Hi ... git pull done !!!
