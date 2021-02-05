6b01173e27731b506d16ad73dc67d09e30879801
Angel Casado <acasadoalonso@gmail.com>
2021-02-05 16:42:44 +0100

    sync

--- provisioning/SWIFACE.sh
+++ provisioning/SWIFACE.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "DROP DATABASE IF EXISTS SWIFACE "       | mysql  -h 172.17.0.2 -u root -pogn 
 echo "CREATE DATABASE IF NOT EXISTS SWIFACE " | mysql  -h 172.17.0.2 -u root -pogn 
-mysql SWIFACE -h 172.17.0.2 -u root -pogn </tmp/DBschema.sql
+mysql SWIFACE -h 172.17.0.2 -u root -pogn </tmp/SWIFACE.sql
