13c1b77aa9e219b77c1b01058eff4f2ef94897b1
Angel Casado <acasadoalonso@gmail.com>
2021-09-28 20:48:35 +0200

    minor adjustments

--- dockerfiles/SWidaily.docker.sh
+++ dockerfiles/SWidaily.docker.sh
@@ -14,22 +14,26 @@ cd $DBpath
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 echo "                   " 				>>SWproc.docker.log
-echo "Process Mariadb DB." 				>>SWproc.docker.log
-echo "==================." 				>>SWproc.docker.log
+echo "SWiface: end of day process using MariaDB." 	>>SWproc.docker.log
+echo "=========================================="	>>SWproc.docker.log
+echo "                   " 				>>SWproc.docker.log
+date			 				>>SWproc.docker.log
 echo "                   " 				>>SWproc.docker.log
 docker stop swiface 	 				>>SWproc.docker.log
+echo "                   " 				>>SWproc.docker.log
 mysqlcheck -u $DBuser -p$DBpasswd -h $server SWIFACE   	>>SWproc.docker.log
 mysqlcheck -u $DBuser -p$DBpasswd -h $server SWARCHIVE 	>>SWproc.docker.log
-echo "Set session      " 				>>SWproc.docker.log
-echo "set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'; "| mysql   -v -h $server -u $DBuser -p$DBpasswd SWIFACE	      >>SWproc.docker.log
+echo "Set session ...     " 				>>SWproc.docker.log
+echo "set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'; "| mysql   -v -h $server -u $DBuser -p$DBpasswd SWIFACE	      						   >>SWproc.docker.log
 echo "select 'Number of fixes: on the DB:', count(*) from OGNDATA; select station, 'Kms.max.:',max(distance),'        Flarmid :',idflarm, 'Date:',date, time, station from OGNDATA group by station; "  | mysql   -v -h $server -u $DBuser -p$DBpasswd SWIFACE	   >>SWproc.docker.log
-echo "mysqldump       " 				>>SWproc.docker.log
+echo "mysqldump  ...       " 				>>SWproc.docker.log
 mysqldump  -u $DBuser -p$DBpasswd --add-drop-table -h $server SWIFACE OGNDATA >ogndata.sql 
 mysql      -u $DBuser -p$DBpasswd                  -h $server SWARCHIVE       <ogndata.sql 	>>SWproc.docker.log
-echo "delete from OGNDATA   " 									>>SWproc.docker.log
+echo "delete from OGNDATA ...  " 								>>SWproc.docker.log
 echo "delete from OGNDATA;" | mysql -u $DBuser -p$DBpasswd     -v -h $server SWIFACE       	>>SWproc.docker.log
 mv ogndata.sql archive
 docker logs --timestamps --details --since $(date +%Y-%m-%d) swiface 				>>SWproc.docker.log
-echo "End of processes  Mariadb DB at server: "$hostname $(date) $HOSTNAME $city		>>SWproc.docker.log
+echo "End of processes  MariaDB at server: "$hostname $(date) $HOSTNAME $city			>>SWproc.docker.log
+echo "=========================================================================="		>>SWproc.docker.log
 mv SWproc.docker.log archive/SWiproc.docker$(date +%y%m%d).log
 cd
--- dockerfiles/swiface.start.sh
+++ dockerfiles/swiface.start.sh
@@ -10,10 +10,9 @@ location=$(echo  `grep '^location_name '   $CONFIGDIR/SWSconfig.ini` | sed 's/=/
 cd $DBpath
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-echo "Starting docker container swiface at: "$HOSTNAME $(date) >>SWproc.docker.log
-echo "=======================================================" >>SWproc.docker.log
-echo $location >>SWproc.docker.log
-
 city='Sisteron'
-docker start swiface >>SWproc.docker.log
-/bin/echo '/bin/bash ~/src/SWSsrc/dockerfiles/SWidaily.docker.sh '$city | at -M $(calcelestial -n -p sun -m set -q $city -H civil) + 15 minutes
+echo "Starting docker container swiface at: "$HOSTNAME $(date) 	>>SWproc.docker.log
+echo "=======================================================" 	>>SWproc.docker.log
+echo "Location: "$location "City: "$city 			>>SWproc.docker.log
+docker start swiface 						>>SWproc.docker.log
+/bin/echo '/bin/bash ~/src/SWSsrc/dockerfiles/SWidaily.docker.sh '$city | at -M $(calcelestial -n -p sun -m set -q $city -H civil) 
