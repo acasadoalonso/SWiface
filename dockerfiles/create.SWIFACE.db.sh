e1efc6f1077d225ec275ee29bfce4af1678ad98e
Angel Casado <acasadoalonso@gmail.com>
2021-10-23 23:28:11 +0200

    sync

--- dockerfiles/create.SWIFACE.db.sh
+++ dockerfiles/create.SWIFACE.db.sh
@@ -1,24 +1,23 @@
-!#/bin/bash
-bash mariadbdbnet.sh
-arch=$(uname -m)
-if [ $arch != 'x84-64' ]
-then
-    cd Mariadb.debian
-    bash mariadb.patch
-    make
-    bash mariadb.sh
-    bash mariadbpma.sh
-    cd ..
-else
-    bash mariadb.sh
-    bash mariadbpma.sh
+#!/bin/bash
+
+if [ -z $CONFIGDIR ]
+then 
+     export CONFIGDIR=/etc/local/
 fi
-bash mariadb.start
-echo "Create the MySQL database SWIFACE "			#
-echo "Type the PASSword for the MySQL database SWIFACE "	#
-echo "========================================================" #
-echo "CREATE DATABASE SWIFACE" | mysql -h mariadb -u root -p	#
-mysql -u root -p -h mariadb --database SWIFACE <DBschema.sql	#
+DBuser=$(echo    `grep '^DBuser '   $CONFIGDIR/SWSconfig.ini` | sed 's/=//g' | sed 's/^DBuser //g')
+DBpasswd=$(echo  `grep '^DBpasswd ' $CONFIGDIR/SWSconfig.ini` | sed 's/=//g' | sed 's/^DBpasswd //g' | sed 's/ //g' )
+DBpath=$(echo    `grep '^DBpath '   $CONFIGDIR/SWSconfig.ini` | sed 's/=//g' | sed 's/^DBpath //g' | sed 's/ //g' )
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+echo "Create the MySQL OGN user "				#
+echo "Type the PASSword for the MySQL database "	        #
+echo "GRANT ALL PRIVILEGES ON *.* TO 'ogn'@'%' IDENTIFIED BY '$DBpasswd'; " | mysql -u root -p$1 -h mariadb    #
+echo "GRANT SELECT ON *.* TO 'ognread'@'%'     IDENTIFIED BY '$DBpasswd'; " | mysql -u root -p$1 -h mariadb    #
 echo " "							#
-echo "Make the swiface docker container "			#
-make
+echo "Create the MySQL database SWIFACE "			#
+echo "========================================================" #
+echo "CREATE DATABASE SWIFACE" | mysql -h mariadb -u root -p$1	#
+mysql -u root -p$1 -h mariadb --database SWIFACE <DBschema.sql	#
+echo " "							#
