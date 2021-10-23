6b7bd24f3ecd188c340ca421887b8f901a46bf06
Angel Casado <acasadoalonso@gmail.com>
2021-10-23 20:57:10 +0200

    sync

--- dockerfiles/swiface.standalone.sh
+++ dockerfiles/swiface.standalone.sh
@@ -1,1 +1,1 @@
-docker run --net mynetsql --ip 172.18.0.4 --name swiface --restart unless-stopped -d swiface:latest
+docker run --net mynetsql --ip 172.18.0.4 --name swiface --add-host mariadb:172.18.0.2 --restart unless-stopped -d swiface:latest
