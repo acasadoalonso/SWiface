12493e10f2419e130cfb7edc60fb6b806a662e61
Angel Casado <acasadoalonso@gmail.com>
2023-09-01 08:19:47 +0200

    GPS = NO is no GPS error

--- SWiface.py
+++ SWiface.py
@@ -717,10 +717,9 @@ try:
                 rot = 0
             sensitivity = msg['sensitivity']
             gps = 	msg['gps']
-            if gps == 'NO' or gps == 'NO ':
-               continue
-            if datal(gps,'x').isnumeric() and datar(gps,'x').isnumeric() and (int(datal(gps,'x')) > 10 or int(datar(gps,'x'))) > 10:
-                continue  				# bad quality GPS data         
+            if gps != 'NO' and gps != 'NO ':		# check GPS quality
+               if datal(gps,'x').isnumeric() and datar(gps,'x').isnumeric() and (int(datal(gps,'x')) > 10 or int(datar(gps,'x'))) > 10:
+                  continue  				# bad quality GPS data         
             hora = 	msg['time']			# fix time
             if source == 'DLYM':
                 dly = timedelta(seconds=DELAY)		# add DELAY
