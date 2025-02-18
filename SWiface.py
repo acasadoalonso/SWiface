f59bf4717e85641268544b4bdf6050698fa703f1
acasadoalonso <acasado@acm.org>
2025-02-18 11:17:46 +0100

    typo again

--- SWiface.py
+++ SWiface.py
@@ -528,7 +528,7 @@ try:
         if local.hour > duskTime.hour:	# check if we are beyond the DUSK
 
             print("At Sunset now ... Time is (server):", date, 
-                  "UTC. Location time:", location.date, 
+                  "UTC. Location time:", local.date, 
                   "\nlocal hour", local.hour, 
                   "Local time:", local, 
                   "\nDusk time", duskTime, duskTime.hour,
